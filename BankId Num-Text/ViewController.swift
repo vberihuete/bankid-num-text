a1e22fc670a6e4b11cda0b232e972f3cf19ee3e5
Vincent Berihuete <vincent.berihuete@daresay.co>
2019-07-16 14:46:38 +0200

    Initial Commit

---
+++ BankId Num-Text/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BankId Num-Text
+//
+//  Created by Vincent Berihuete on 2019-07-16.
+//  Copyright © 2019 Vincent Berihuete. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
